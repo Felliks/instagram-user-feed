2e97d138b7cfaa085689d77df9dea53d317c5468
XyZ <935908-xyz.ambassador@users.noreply.gitlab.com>
2022-09-27 12:21:38 +0300

    Fixes

--- src/Instagram/Auth/Checkpoint/ImapClient.php
+++ src/Instagram/Auth/Checkpoint/ImapClient.php
@@ -135,7 +135,7 @@ class ImapClient
                 }
 
                 if ($isMailFromInstagram && isset($match[1])) {
-                    imap_delete($resource, $i);
+                    imap_delete($resource, (string) $i);
 
                     $foundCode = true;
                     $code      = $match[1];
