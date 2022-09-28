19d8678e4dbffb7002af3d31dcd79c6d7922faba
XyZ <935908-xyz.ambassador@users.noreply.gitlab.com>
2022-09-28 03:51:30 +0300

    Fixes

--- src/Instagram/Auth/Login.php
+++ src/Instagram/Auth/Login.php
@@ -182,14 +182,7 @@ class Login
         $challenge = new Challenge($this->client, $cookieJar, $data->checkpoint_url, $this->challengeDelay);
 
         $challengeContent = $challenge->fetchChallengeContent();
-
-        var_dump($challengeContent);
-        var_dump($challengeContent->Challenge);
-        var_dump($challengeContent->Challenge[0]);
-        var_dump($challengeContent->Challenge[0]->fields);
-        var_dump($challengeContent->Challenge[0]->fields->enrollment_reason);
-
-        $reason = $challengeContent->Challenge[0]->fields->enrollment_reason;
+        $reason = $challengeContent->entry_data->Challenge[0]->fields->enrollment_reason;
 
         if (str_contains($reason, 'USR user data scraping')) {
             throw new InstagramBlockAccountException($reason);
